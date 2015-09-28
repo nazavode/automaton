b8a24ad89d4dc2358023dfcefa089ffec8d6a75d
Federico Ficarelli <toroidh@gmail.com>
2015-09-28 12:10:16 +0200

    Added source skeleton.

--- automaton/automaton.py
+++ automaton/automaton.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2013 Federico Ficarelli
+# Copyright 2015 Federico Ficarelli
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,14 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""
-Minimal finite-state machine.
-"""
-
-__author__ = 'Federico Ficarelli'
-__copyright__ = 'Copyright (c) 2015 Federico Ficarelli'
-__license__ = 'Apache License Version 2.0'
 
 
 __all__ = ()
