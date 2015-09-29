fe072febb87b28e9aad17cf9ef3d657b3b3d7dd0
Federico Ficarelli <toroidh@gmail.com>
2015-09-29 12:33:40 +0200

    Addes proper tests for connection check.

--- tests/test_automaton.py
+++ tests/test_automaton.py
@@ -62,7 +62,7 @@ def test_initial_state():
         assert noinit_obj.state == state
 
 
-def test_unconnected():
+def test_unconnected_naive():
     # The graph representing the FSM must be connected.
     with pytest.raises(DefinitionError):
         class Unconnected(Automaton):
@@ -70,18 +70,71 @@ def test_unconnected():
             cluster2 = Event("state_c", "state_d")
 
 
-def test_multiple_events():
+def test_unconnected():
+    # The graph representing the FSM must be connected.
+    with pytest.raises(DefinitionError):
+        class Unconnected(Automaton):
+            cluster1_1 = Event("state_a", "state_b")
+            cluster1_2 = Event("state_b", "state_c")
+            cluster1_3 = Event("state_c", "state_d")
+            #
+            cluster2_1 = Event("state_e", "state_f")
+            cluster2_2 = Event("state_f", "state_g")
+            cluster2_3 = Event("state_g", "state_h")
+            #
+            cluster3_1 = Event("state_1", "state_2")
+            cluster3_2 = Event("state_2", "state_3")
+            cluster3_3 = Event("state_3", "state_4")
+            cluster3_4 = Event("state_4", "state_5")
+            cluster3_5 = Event("state_5", "state_6")
+            cluster3_6 = Event("state_6", "state_7")
+
+
+def test_connected_clusters():
+    # Connect the clusters:
+    class Unconnected(Automaton):
+        cluster1_1 = Event("state_a", "state_b")
+        cluster1_2 = Event("state_b", "state_c")
+        cluster1_3 = Event("state_c", "state_d")
+        #
+        cluster2_1 = Event("state_e", "state_f")
+        cluster2_2 = Event("state_f", "state_g")
+        cluster2_3 = Event("state_g", "state_h")
+        #
+        cluster3_1 = Event("state_1", "state_2")
+        cluster3_2 = Event("state_2", "state_3")
+        cluster3_3 = Event("state_3", "state_4")
+        cluster3_4 = Event("state_4", "state_5")
+        cluster3_5 = Event("state_5", "state_6")
+        cluster3_6 = Event("state_6", "state_7")
+        #
+        conn_1_2 = Event("state_d", "state_e")
+        conn_2_3 = Event("state_h", "state_1")
+
+
+def test_multiple_arcs():
     # It's ok to have multiple directed arcs between two states,
     # it seems like an elegant (but somehow not efficient) way
     # to support multiple events per transition.
-    class MultipleEvents(Automaton):
+    class Multiple(Automaton):
         event1 = Event("state_a", "state_b")
         event2 = Event("state_a", "state_b")
         event3 = Event("state_a", "state_b")
 
-    assert "event1" in MultipleEvents.events
-    assert "event2" in MultipleEvents.events
-    assert "event3" in MultipleEvents.events
+    assert "event1" in Multiple.events
+    assert "event2" in Multiple.events
+    assert "event3" in Multiple.events
+
+
+def test_auto_arc():
+    class Auto(Automaton):
+        loop = Event("state_a", "state_a")
+
+    assert "loop" in Auto.events
+    auto_obj = Auto(initial_state="state_a")
+    assert auto_obj.state == "state_a"
+    auto_obj.event("loop")
+    assert auto_obj.state == "state_a"
 
 
 def test_transition():
